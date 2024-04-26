20a0b6e17d5c727338e8542eb91b9b341d61b7ef
Mukesh Prajapati <109510553+Mukeshp405@users.noreply.github.com>
2024-04-26 16:29:07 +0530

    Update Program_1_Add_two_numbers.java

--- Program_1_Add_two_numbers.java
+++ Program_1_Add_two_numbers.java
@@ -1,6 +1,6 @@
 public class Program_1_Add_two_numbers {
     public static void main(String[] args){
-//        Declaretion of variable and values
+//        Declaring the variable and values
         int num1 = 4,  num2 = 2, sum;
 
 //        Adding two variables(num1, num2) and stored into another variable(sum)
