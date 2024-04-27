2be90e101858aca0423fc8d78ddc80381ebddacf
Mukesh Prajapati <109510553+Mukeshp405@users.noreply.github.com>
2024-04-27 22:38:12 +0530

    Add files via upload

---
+++ Program/Program_1_Add_two_numbers.java
@@ -0,0 +1,12 @@
+public class Program_1_Add_two_numbers {
+    public static void main(String[] args){
+//        Declaretion of variable and values
+        int num1 = 4,  num2 = 2, sum;
+
+//        Adding two variables(num1, num2) and stored into another variable(sum)
+        sum = num1 + num2;
+
+//        Displaying the result
+        System.out.println("The sum of two numbers is :- " + sum);
+    }
+}
